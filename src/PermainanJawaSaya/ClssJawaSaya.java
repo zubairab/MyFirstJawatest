439cc8e9223d207932d90f107e3f9061d493b673
zubairab <35870327+zubairab@users.noreply.github.com>
2020-04-04 18:41:09 +0800

    Update ClssJawaSaya.java

--- src/PermainanJawaSaya/ClssJawaSaya.java
+++ src/PermainanJawaSaya/ClssJawaSaya.java
@@ -1,7 +1,7 @@
 package PermainanJawaSaya;
 
 public class ClssJawaSaya {
-
+// Testing import from GIt
     public static void main(String[] val)
     {
         System.out.println("Selamat Petang");
